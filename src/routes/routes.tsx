5f22388b2f178b2958950220415c1efaeaff2dc7
joseerisvaldo <joseerisvaldopg@gmail.com>
2026-04-16 00:17:48 -0300

    feat: add public and private route components for authentication handling

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,11 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "../features/auth/presentation/login-page.view";
+import { PrivateRoute } from "./private-route";
+import { PublicRoute } from "./public-route";
+
+// exemplo
+function DashboardPage() {
+  return <h1>Dashboard</h1>;
+}
 
 export default function RoutesApp() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route element={<PublicRoute />}>
+          <Route path="/login" element={<LoginPage />} />
+        </Route>
+
+        <Route element={<PrivateRoute />}>
+          <Route path="/" element={<DashboardPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
