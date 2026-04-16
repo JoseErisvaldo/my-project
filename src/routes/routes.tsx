641770815b4fed0af939497e7a3971676b50ec58
joseerisvaldo <joseerisvaldopg@gmail.com>
2026-04-15 22:07:51 -0300

    initial commit

---
+++ src/routes/routes.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+
+export default function RoutesApp() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={""} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
